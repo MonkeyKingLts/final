866b1fa1186b72420b5c08a7c78ba4b2f0b70d36
孙狗宝 <1829889340@qq.com>
2026-05-17 20:20:21 +0800

    fix-bugs

--- src/tools/SummaryCard.vue
+++ src/tools/SummaryCard.vue
@@ -1,54 +1,51 @@
 <template>
-    <div
-      class="w-full  h-[120px]
-             rounded-[25px] bg-white
-             flex items-center gap-6
-             px-7"
-    >
-      <!-- icon -->
+  <div
+    class="w-full h-[120px] rounded-[25px] bg-white grid grid-cols-[150px_1fr] items-center px-7"
+  >
+    <!-- icon area -->
+    <div class="flex justify-center">
       <div
-        class="size-[70px] rounded-full
-               flex items-center justify-center
-               shrink-0"
+        class="size-[70px] rounded-full flex items-center justify-center"
         :class="iconBg"
       >
-        <i :class="[icon, iconColor, 'text-[30px]']"></i>
-      </div>
-  
-      <!-- text -->
-      <div class="flex flex-col">
-        <span class="text-[#718EBF] text-[18px] font-semibold">
-          {{ title }}
-        </span>
-  
-        <span class="text-[#343C6A] text-[24px] font-bold">
-          {{ value }}
-        </span>
+        <i :class="[icon, iconColor, 'fa-fw text-[30px] leading-none']"></i>
       </div>
     </div>
-  </template>
-  
-  <script setup>
-  defineProps({
-    icon: {
-      type: String,
-      default: 'fa-solid fa-user',
-    },
-    iconBg: {
-      type: String,
-      default: 'bg-[#E7EDFF]',
-    },
-    iconColor: {
-      type: String,
-      default: 'text-[#396AFF]',
-    },
-    title: {
-      type: String,
-      default: '个人贷款',
-    },
-    value: {
-      type: String,
-      default: '$50,000',
-    },
-  })
-  </script>
+
+    <!-- text -->
+    <div class="flex flex-col">
+      <span class="text-[#718EBF] text-[18px] font-semibold">
+        {{ title }}
+      </span>
+
+      <span class="text-[#343C6A] text-[24px] font-bold">
+        {{ value }}
+      </span>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  icon: {
+    type: String,
+    default: "fa-solid fa-user",
+  },
+  iconBg: {
+    type: String,
+    default: "bg-[#E7EDFF]",
+  },
+  iconColor: {
+    type: String,
+    default: "text-[#396AFF]",
+  },
+  title: {
+    type: String,
+    default: "个人贷款",
+  },
+  value: {
+    type: String,
+    default: "$50,000",
+  },
+});
+</script>
